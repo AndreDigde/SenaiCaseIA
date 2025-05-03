d637abd7d08d0b571746bb177d70e2175197e0aa
André Santos <andresantos@MacBook-Pro-de-Andre.local>
2025-05-03 13:52:53 -0300

    ajustes de importação dos scripts

--- AT2/create_model.py
+++ AT2/create_model.py
@@ -1,6 +1,5 @@
 from ucimlrepo import fetch_ucirepo
-from eval_models import clear_outliers
-from regression_train import train_regression, shuffle_and_split_data, save_results
+from regression_train import train_regression, shuffle_and_split_data, save_results, clear_outliers
 from sklearn.gaussian_process import GaussianProcessRegressor
 import pickle
 import os
@@ -11,7 +10,7 @@ energy_efficiency = fetch_ucirepo(id=242)
 features = energy_efficiency.data.features 
 targets = energy_efficiency.data.targets
 results = []
-features_cleared, targets_cleared = clear_outliers(features, targets, 2.5, [])
+features_cleared, targets_cleared, _ = clear_outliers(features, targets, 2.5)
 x_train, x_test, y_train, y_test = shuffle_and_split_data(features, targets)
 model = train_regression(GaussianProcessRegressor(), "trainned_model", 0, x_train, x_test, y_train, y_test, results)
 
@@ -19,4 +18,4 @@ if os.path.exists(_RESULTS_PATH):
         os.remove(_RESULTS_PATH)
 
 save_results([["train_label, mse, rmse, mae, r2"], results], _RESULTS_PATH)
-pickle.dump(model, open("t_model.pkl", 'wb'))
+pickle.dump(model, open("t_model.pkl", "wb"))
